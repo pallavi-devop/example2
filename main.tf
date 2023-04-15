08e80cc96e159276f66446cb862aa5a60ce1e658
root <root@ip-172-31-32-33.ap-northeast-1.compute.internal>
2023-04-15 10:56:33 +0000

    this is terraform file

---
+++ main.tf
@@ -0,0 +1,61 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region     = "ap-northeast-1"
+  access_key = "AKIAXOBVO4FVAMMMEF6R"
+  secret_key = "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"
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "myvpc"
+  }
+}
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    Name = "pub_subnet"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "my-igw"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+  tags = {
+    Name = "pub-rt"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_asso" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_instance" "mysec2" {
+  ami           = "ami-0d979355d03fa2522"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnet.id
+
+}
+
