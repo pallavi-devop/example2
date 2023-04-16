8a96322dc7cf3f74ce189590373bfd6c70a50f4c
Pallavi Pramod Kawade <121016747+pallavi-devop@users.noreply.github.com>
2023-04-16 14:43:56 +0530

    commit
    Signed-off-by: Pallavi Pramod Kawade <121016747+pallavi-devop@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
   secret_key = "gwl3qZyH60JL6OuWUhZkMJYjKaSNUBxrR6BB5tSS"
 }
 
-resource "aws_vpc" "my_vpc" {
+resource "aws_vpc" "my_vpc1" {
   cidr_block = "10.0.0.0/16"
   tags = {
     Name = "myvpc"
